36eb13247d96b4ad0ff9e47d27e970c25355f5a0
kiransaiveerandi <kiransaiveerandi@gmail.com>
2026-01-30 12:45:29 +0530

    shell

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-person1=$1
-person2=$2
+# person1=$1
+# person2=$2
 
-echo "$person1: hi $person2"
-echo "$person2: hi $person1"
+# echo "$person1: hi $person2"
+# echo "$person2: hi $person1"
+
+echo "enter your password "
+read PASSWORD
+echo "password is : $PASSWORD"
+
 
 
