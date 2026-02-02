5d6fe1a897378c2b340724c849a04eef9938674f
kiransaiveerandi <kiransaiveerandi@gmail.com>
2026-02-02 12:03:09 +0530

    shell

--- 01-variables.sh
+++ 01-variables.sh
@@ -5,22 +5,30 @@
 # echo "$person1: hi $person2"
 # echo "$person2: hi $person1"
 
+#read variable
+----------------
 # echo "enter your password "
-# read -s PASSWORD
+# read -s PASSWORD (-s is used for hide the pass or pin)
 # echo "password is : $PASSWORD"
-
+#Environment variable
+------------------------
 # echo "name is: $name"
 
-# DATE=$(date)
+# This variable is valid until this process is alive, it is accessible to sub prosess also
+# By using "Export" 
+# Once process is not alive there is a chance to access in user home location, open the".bashrc"file enter the value, after that apply the changes by using "source.bashrc" command
 
+# Dynamic execution
+------------------------
+# DATE=$(date)
 # echo "timestamp excuted : $DATE"
 
-starting_time=$(date +%s)
+# starting_time=$(date +%s)
+# sleep 10
+# ending_time=$(date +%s)
+# total_time=$(($ending_time-$starting_time))
+# echo "executed time : $total_time"
 
-sleep 10
-
-ending_time=$(date +%s)
-
-total_time=$(($ending_time-$starting_time))
-
-echo "executed time : $total_time"
+special variable
+------------------
+echo "all variables passed to the script:$@ "
