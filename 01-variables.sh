6b26168613b57c9e87575731e0dff3033a1b34ad
kiransaiveerandi <kiransaiveerandi@gmail.com>
2026-02-04 21:25:09 +0530

    shell

--- 01-variables.sh
+++ 01-variables.sh
@@ -81,14 +81,14 @@ USERID=$(id -u)
 
 if [ $USERID -eq 1 ]; then
    echo "ERROR: please run this script with root privelege"
-   
+   Exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
    echo "ERROR: installing mysql is failure"
-   
+   Exit 1
 else 
    echo "installing mysql is SUCCESS"
 fi
