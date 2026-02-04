b072d173c90fe0043e226129427c50adf03b532b
kiransaiveerandi <kiransaiveerandi@gmail.com>
2026-02-04 18:48:55 +0530

    shell

--- 01-variables.sh
+++ 01-variables.sh
@@ -51,20 +51,20 @@
 # conditions---------
 NUM=$1
 
-# if [ $NUM -lt 20 ]; then
-#    echo "given number $NUM is less then to the 20"
-# else
-#    echo "given number $NUM is greter then to the 20"
-# fi
-
-
 if [ $NUM -lt 20 ]; then
    echo "given number $NUM is less then to the 20"
-elif 
-   echo "given number $NUM is equal to the 20"
 else
    echo "given number $NUM is greter then to the 20"
 fi
+
+
+# if [ $NUM -lt 20 ]; then
+#    echo "given number $NUM is less then to the 20"
+# elif [ $NUM -eq 20 ]; then
+#    echo "given number $NUM is equal to the 20"
+# else
+#    echo "given number $NUM is greter then to the 20"
+# fi
 
 
 # echo "enter number:"
