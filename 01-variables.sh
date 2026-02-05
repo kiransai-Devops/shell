feac94e7ac06639b2798ec4e0decb565b1e20ad6
kiransaiveerandi <kiransaiveerandi@gmail.com>
2026-02-05 12:45:04 +0530

    shell

--- 01-variables.sh
+++ 01-variables.sh
@@ -175,7 +175,7 @@ if [ $USERID -eq 1 ]; then
 fi
 
 VALIDATE () { # functions revive inputs through args just like shell script args
-    if [ $1 -ne 0 ]; then
+    if [ $1 -eq 1 ]; then
         echo -e "installing $2 is  $R failure $N" | tee -a $LOG_FILE
         exit 1
    else 
@@ -184,7 +184,7 @@ VALIDATE () { # functions revive inputs through args just like shell script args
 }
 
 dnf list installed mysql &>>$LOG_FILE
-if [ $? -ne 0 ]; then
+if [ $? -eq 1 ]; then
     dnf install mysql -y &>>$LOG_FILE
     VALIDATE $? "mysql"
 else
@@ -192,7 +192,7 @@ else
 fi
 
 dnf list installed nginx &>>$LOG_FILE
-if [ $? -ne 0 ]; then
+if [ $? -eq 1 ]; then
     dnf install nginx -y &>>$LOG_FILE
     VALIDATE $? "nginx"
 else
